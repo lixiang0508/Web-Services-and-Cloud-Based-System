985c6ba4af2b2619b132be3b9395b2ac2f0e24cd
lixiang0508 <79568031+lixiang0508@users.noreply.github.com>
2023-04-10 21:17:27 +0200

    Update app.py

--- assignment1-prac/app.py
+++ assignment1-prac/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request
-from static.resources import dict_map,reverse_map,idx
+#from urllib import request
+
+from flask import Flask,request
+from static.resources import dict_map,reverse_map
+from utils import build_result,is_valid_url,generate_unique_id
 app = Flask(__name__)
+
 
 #dict_map = {}  # id : url
 #reverse_map = {}  # url :id
 
 
-def build_result(code, value):
-    return {"code": code, "value": value}
+#def build_result(code, value):
+    #return {"code": code, "value": value}
 
 
 
@@ -15,11 +19,11 @@ def build_result(code, value):
 def generate_id(url):
     #generate id based on the given url
     # whatever algorithm to generate an id
-    global idx
+    #global idx
     #cur_id = g.get('idx')
     #g.idx= g.idx+1
-    idx+=1
-    return idx
+    #idx+=1
+    return generate_unique_id()
 
 
 # 请求方式
@@ -27,19 +31,24 @@ def generate_id(url):
 def get_id(id):
     if int(id) not in dict_map.keys():
         print("not found this id")
-        return build_result(404, "")
+        return build_result(404, "This id does not exist")
     return build_result(301, dict_map[int(id)])
 
 
 @app.route('/<id>', methods=['PUT'])
 def put_id_url(id):
-    #generate
+    if "url" not in request.form:
+        return build_result(400, "No url passed in ")
+
     if int(id) not in dict_map.keys(): # this id doesn't exist
-        return build_result(404,"")
-    request_data = request.get_json()
-    url = request_data['url']
+        return build_result(404,"This id does not exist")
+
+    url = request.form['url']
+
     if url in reverse_map.keys(): # the given url exists
-        return build_result(400,"error")
+        return build_result(400,"error the given url has already existed")
+    if not is_valid_url(url):
+        return build_result(400, "error the given url is not valid")
     dict_map[int(id)]=url
     reverse_map[url]=int(id)
     return build_result(200, "")
@@ -48,7 +57,7 @@ def put_id_url(id):
 @app.route('/<id>', methods=['DELETE'])
 def delete_id(id):
     if int(id) not in dict_map.keys():
-        return build_result(404, "")
+        return build_result(404, "This id does not exist")
     url = dict_map[int(id)]
     del dict_map[int(id)]
     del reverse_map[url]
@@ -62,8 +71,15 @@ def get_empty():
 
 @app.route('/<url>', methods=['POST'])
 def post_url(url):
+    #if "url" not in request.form:
+        #return build_result(400, "No url passed in ")
+    #url = request.form['url']
+    if not is_valid_url(url):
+        return build_result(400, "error the given url is not valid")
     if url in reverse_map.keys():
-        return build_result(400, "error")
+        return build_result(400, "error! This url has already existed")
+    if not is_valid_url(url):
+        return build_result(400, "error! This url is not valid")
     new_id = generate_id(url)
     reverse_map[url] = new_id
     dict_map[new_id] = url
@@ -74,19 +90,20 @@ def post_url(url):
 
 @app.route('/', methods=['DELETE'])
 def delete_empty():
+    #TODO we have to identify the user and delete his own id-url pairs
     return build_result(404, "")
 
 
-@app.route('/checkmap')
+
 def check_map():
     return str(dict_map)
 
 
-@app.route('/check_reversemap')
+
 def check_reversemap():
     return str(reverse_map)
 
 
 if __name__ == '__main__':
-    idx=0
+    #idx=0
     app.run()
